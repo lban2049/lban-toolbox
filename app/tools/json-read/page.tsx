e375981a8c9a7bffb64b667c88fef14acf2cf8ef
LBan <hanxl5123@163.com>
2023-10-20 08:12:33 +0000

    添加 json 查看器页面

---
+++ app/tools/json-read/page.tsx
@@ -0,0 +1,9 @@
+export default function JsonRead() {
+  return (
+    <div className="p-5">
+      <h1 className="text-3xl font-medium border-b pb-2 pl-2 border-black-400">
+        json 在线阅读查看器 (Online JSON Viewer)
+      </h1>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        black: {
+          "50": "#f6f6f6",
+          "100": "#e7e7e7",
+          "200": "#d1d1d1",
+          "300": "#b0b0b0",
+          "400": "#888888",
+          "500": "#6d6d6d",
+          "600": "#5d5d5d",
+          "700": "#4f4f4f",
+          "800": "#454545",
+          "900": "#3d3d3d",
+          "950": "#000000",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
